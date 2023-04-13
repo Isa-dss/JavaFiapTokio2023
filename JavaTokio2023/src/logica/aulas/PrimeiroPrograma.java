5567fc892bfcec4651ae0980136bfcffdbdee23b
isa-dss <isabella.santana@aluno.ifsp.edu.br>
2023-04-13 18:24:59 -0300

    primeiro commit java lógica

---
+++ JavaTokio2023/src/logica/aulas/PrimeiroPrograma.java
@@ -0,0 +1,34 @@
+package logica.aulas;
+
+// sysout
+//Ctrl+F11 executa automaticamente 
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World :) ");
+
+		int idade;
+		idade = 23;
+        System.out.println(idade);
+       
+       idade = 42;
+       idade++;
+       System.out.println(idade);
+       
+       double salario =1318.20;
+       System.out.println(salario);
+       
+       String nome = "isa";
+       System.out.println(nome);
+       
+       System.out.println("bem vinda, " + nome + "!");
+       System.out.println(nome + ",você tem " + idade + "anos");
+       
+       
+       
+       
+	}
+
+}
---
+++ JavaTokio2023/src/logica/aulas/SegundoPrograma.java
@@ -0,0 +1,21 @@
+package logica.aulas;
+
+import java.util.Scanner;
+
+public class SegundoPrograma {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner (System.in);
+		
+		System.out.println("Digite seu nome:");
+		String nome = entrada.nextLine();
+		
+		System.out.println("Digite a sua idade:");
+		System.out.println("Sua idade e:" + entrada.nextLine());
+		
+		System.out.println("Bem vindo, " + nome + " :)");
+
+
+ }
+}
